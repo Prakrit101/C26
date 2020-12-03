11ff412ff663b4923c212a0b91728876b7add98d
Shayanti Saha <shayantimachome@Shayantis-MacBook-Pro.local>
2020-12-03 14:46:20 -0500

    added a new box

--- sketch.js
+++ sketch.js
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    box6 = new Box(900,160,70,70)
     bird = new Bird(100,100);
 
 }
@@ -58,7 +58,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    box6.display()
     bird.display();
     platform.display();
 }
